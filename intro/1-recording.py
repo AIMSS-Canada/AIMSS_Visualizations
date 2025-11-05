a404ed2f2e0cc60124191b3288fb1116d0daf640
Ying Wan <90426228+Ying-SunWan@users.noreply.github.com>
2025-11-05 12:24:20 -0700

    upload video link

--- intro/1-recording.py
+++ intro/1-recording.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.set_page_config(layout="centered")
 
-# st.video("https://youtu.be/lHgsQ6VacQo?si=3owhpOoUecpq1s_3")
+st.video("https://youtu.be/imKcIst-G9M?si=qEyV-vuEmK6uLiif")
