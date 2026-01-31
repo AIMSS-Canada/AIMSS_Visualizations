96dc780dd75b99bad5abbefb33d9a97a271330f2
Ying Wan <ywan4@ualberta.ca>
2026-01-30 17:22:24 -0700

    session 3 uploaded

--- ai_algos/3-recording.py
+++ ai_algos/3-recording.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.set_page_config(layout="centered")
 
-st.video("https://youtu.be/imKcIst-G9M?si=qEyV-vuEmK6uLiif")
+st.video("https://youtu.be/SLlSAbbF5gQ?si=wJQYKpRBB2u0v91N")
