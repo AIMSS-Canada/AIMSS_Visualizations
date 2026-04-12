8c390b89e488b9395781c976152fbcb2f486a7c2
Ying Wan <90426228+Ying-SunWan@users.noreply.github.com>
2026-04-11 20:22:59 -0600

    trying to fix pathing...

--- 1_intro/1-activities.py
+++ 1_intro/1-activities.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import importlib
-dp = importlib.import_module("1_intro.demos.data_preprocessing")
-eval = importlib.import_module("1_intro.demos.evaluation_metrics")
+dp_module = importlib.import_module("1_intro.demos.data_preprocessing")
+eval_module = importlib.import_module("1_intro.demos.evaluation_metrics")
 
 st.set_page_config(layout="centered")
 
 preprocessing_tab, eval_metrics_tab = st.tabs(["Data Preprocessing", "Evaluation Metrics"])
 
 with preprocessing_tab:
-    dp()
+    dp_module.dp()
 with eval_metrics_tab:
-    eval()
+    eval_module.eval()
